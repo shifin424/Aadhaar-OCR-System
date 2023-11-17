91f39cf9727f6c84c446b7592850c4f7ba604124
shifin424 <snehamparakette004@gmail.com>
2023-11-17 21:27:02 +0530

    Added README.md file

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+app.use(morgan("dev"));
 
 app.use('/api/v1',userRouter)
 app.use(errorHandler)
